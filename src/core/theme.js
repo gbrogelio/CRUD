effe817c224f8d45189825c1c69f5659c9cc2bae
gbrogelio <gbrogelio@1go.ph>
2024-02-29 17:45:36 +0800

    First Commit

---
+++ src/core/theme.js
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'react-native-paper';
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#009688',
+    secondary: '#1de9b6',
+    error: '#ff1744',
+  },
+};
